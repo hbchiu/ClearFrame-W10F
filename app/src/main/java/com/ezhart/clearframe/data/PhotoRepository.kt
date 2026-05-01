d6375c312bbe9fbb604a85c59c8694f766da7ed4
hbchiu <hbchiu@gmail.com>
2026-05-01 13:13:14 +0200

    Fix ANR by moving photo hashing to IO dispatcher

--- app/src/main/java/com/ezhart/clearframe/data/PhotoRepository.kt
+++ app/src/main/java/com/ezhart/clearframe/data/PhotoRepository.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import com.ezhart.clearframe.model.Photo
 import java.io.File
 import java.security.MessageDigest
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 private const val TAG = "PhotoRepository"
 
@@ -12,28 +14,24 @@ interface PhotoRepository {
     suspend fun getPhotos(): List<Photo>
 }
 
-class InternalStoragePhotoRepository(private val context: Context) : PhotoRepository{
+class InternalStoragePhotoRepository(private val context: Context) : PhotoRepository {
 
-    override suspend fun getPhotos(): List<Photo> {
-
+    override suspend fun getPhotos(): List<Photo> = withContext(Dispatchers.IO) {
         val storagePath = context.filesDir.path
         val imagePaths: List<String> = context.fileList()
             .filter { f -> f.endsWith("jpg", true) || f.endsWith("mp4", true) || f.endsWith("mov", true) }
             .sortedBy { f -> f }
-            // Initially tried to use Path() for building this path, but got a missing library error
-            // so I just punted and used a template
-            //.map { f -> Path(storagePath, f).toString() }
             .map { f -> "$storagePath/$f" }
 
         val photos = mutableListOf<Photo>()
 
-        for(imagePath in imagePaths){
+        for (imagePath in imagePaths) {
             Log.d(TAG, "Getting hash of $imagePath")
             val digest = File(imagePath).toHash("SHA-1")
             photos.add(Photo(imagePath, digest))
         }
 
-        return photos
+        photos
     }
 
     private fun File.toHash(algorithm: String): String {
@@ -49,4 +47,3 @@ class InternalStoragePhotoRepository(private val context: Context) : PhotoReposi
         return digest.joinToString("") { "%02x".format(it) }
     }
 }
-
